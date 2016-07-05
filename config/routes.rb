f94ee90897089ce24f8f63cbed848a46d55d7d67
sanjay-raj <sanjay.vinzuda@botreetechnologies.com>
2016-07-05 18:45:28 +0530

    product list issuse

--- config/routes.rb
+++ config/routes.rb
@@ -22,14 +22,17 @@ Rails.application.routes.draw do
     get 'store/index'
     get 'store/mycart'
   root :to=> 'store#index', :as => 'store'
+
+
+  resources :products do
+    get :who_bought, on: :member
+  end
     
   end
 
   # get 'list'=>'products#index'
 
-  resources :products do
-    get :who_bought, on: :member
-  end
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
